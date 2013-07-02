43babd6ed609608eac12b2ede088f2f467581a69
Ian A. Mason <Ian.Mason@SRI.com>
2013-07-02 13:13:30 -0700

    Some fair progress on rendezvous.

--- plugin/chrome/rendezvous.js
+++ plugin/chrome/rendezvous.js
@@ -14,11 +14,12 @@
  http://jsperf.com/encoding-xhr-image-data/14
 
 
- *  Step 3: Ask for the status of the onion (should respond with a status and perhaps amn activity)
+ *  Step 3: Ask for the status of the onion (should respond with a status and perhaps an activity)
  *  Step 4: Repeat step 3 until we have a net. Once we have a net we need to do the dance.
  *  Ideas on this Jeroen?
  */
 
+var Rendezvous;
 
 
 
@@ -26,17 +27,119 @@
 
 
 
+Rendezvous = {
+    
+    reset_url: 'http://127.0.0.1:8000/rendezvous/reset',
+    
+    gen_request_url: 'http://127.0.0.1:8000/rendezvous/gen_request',
 
-var rendezvousGenerator = {
+    image_url: 'http://127.0.0.1:8000/rendezvous/image',
+    
+    populate: function() {
+        document.querySelector('#mod_freedom').addEventListener('click', Rendezvous.send_url);
 
-  populate: function () {
- 
+        Rendezvous.reset();
+    },
+    
+    reset:  function() {
+        var reset_request = new XMLHttpRequest();
+        reset_request.onreadystatechange = function () { Rendezvous.reset_response(reset_request); };
+        reset_request.open('GET', Rendezvous.reset_url);
+        //the webRequest API should ignore
+        reset_request.setRequestHeader('DJB_REQUEST', 'true');
+        reset_request.send(null);
+    },
 
+    reset_response: function(request){
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                console.log('Rendezvous.reset OK');
+            } else {
+                console.log('Rendezvous.reset **NOT** OK');
+            }
+        }
+    },
+    
+    send_url: function(){  
+        var address = document.querySelector('#mod_freedom_uri').value;
+        if(!address){
+            Rendezvous.set_status('please enter a mod_freedom server address.'); 
+        } else {
+            //ask the jumpbox to construct our secret and mod_freedom request
+            Rendezvous.gen_request(address); 
+        }
+    },
+
+    set_status: function(msg){
+        if(msg){
+            document.querySelector('#status').innerHTML = 'Status: ' + msg;
+        }
+    },
+
+    gen_request: function(address) {
+        var gen_request = new XMLHttpRequest();
+        gen_request.onreadystatechange = function () { Rendezvous.handle_gen_response(gen_request); };
+        gen_request.open('POST', Rendezvous.gen_request_url);
+        //the webRequest API should ignore
+        gen_request.setRequestHeader('DJB_REQUEST', 'true');
+        gen_request.send(address);
+    },
+    
+    handle_gen_response: function(request){
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                Rendezvous.set_status('Rendezvous.gen_response OK');
+                Rendezvous.handle_response(request.response);
+            } else {
+                Rendezvous.set_status('Rendezvous.gen_response **NOT** OK');
+            }
+        }
+
+    },
+
+    handle_response: function(freedom_uri){
+        var freedom_request = new XMLHttpRequest();
+        freedom_request.onreadystatechange = function () { Rendezvous.process_image(freedom_request); };
+        freedom_request.open('GET', freedom_uri);
+        freedom_request.responseType = 'blob';       
+        freedom_request.send(null);
+    },
+
+    process_image: function(request){
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                Rendezvous.set_status('Freedom response OK: ' + request.getResponseHeader('Content-Type'));
+                Rendezvous.forward_image(request);
+            } else {
+                Rendezvous.set_status('Freedom response  **NOT** OK');
+            }
+        }
+    },
+
+    forward_image: function(response){
+        var image_post = new XMLHttpRequest();
+        image_post.onreadystatechange = function () { Rendezvous.image_post_response(image_post); };
+        image_post.open('POST', Rendezvous.image_url);
+        image_post.setRequestHeader('Content-Type', response.getResponseHeader('Content-Type'));
+        image_post.send(response.response);
+    },
+
+    image_post_response: function(request){
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                Rendezvous.set_status('Image post OK');
+                document.querySelector('#onion_image').src = 'file:///Users/iam/Repositories/isc/jumpbox/twistedServer/onion.jpg';
+            } else {
+                Rendezvous.set_status('Image post **NOT** OK');
+            }
+        }
     }
-  
+
+
+
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-        rendezvousGenerator.populate();
+        Rendezvous.populate();
     });
 
--- twistedServer/rendezvous.py
+++ twistedServer/rendezvous.py
@@ -1,16 +1,33 @@
 from twisted.web.resource import Resource
 
 
-
 class Rendezvous(Resource):
+    #fake everything, till the real thing comes along.
+    server = 'vm06.csl.sri.com'
+    secret = 'U4Sv7k2PY0Gq7TFi'
+    freedom_request = 'http://vm06.csl.sri.com/photos/26907150@N08/1457660969/lightbox?_utma=ACbLoAX643zHB8Bqb5MtfZWLUdfMZDUvH9hthuYoM96yRlIIBtPY1ns1kfEh72EFYUOr0sxHuBs2PiQ4WJf9RVNqCUaaDJabwIv8S5g8Ld1zNhoB4lc8QuqjqjVmk98P9qNJbOBpLQLHTU5Jo1f6koLiS1diUEEpTXRVsWzDKHsA&_utmz=Gu8jdzMURgtpNVnP6odSZVGKBEE='
 
-   def __init__(self, name=None):
-      Resource.__init__(self)
-      self.name = name
 
-   def getChild(self, name, request):
-      return Rendezvous(name)
+    def __init__(self, name=None):
+        Resource.__init__(self)
+        self.name = name
 
-   def render_GET(self, request):
-      print 'GET: %s\n' % self.name
-      return "<html><body>GET: Rendezvous </body></html>"
+    def getChild(self, name, request):
+        return Rendezvous(name)
+
+    def render_GET(self, request):
+        print 'GET: %s\n' % self.name
+        return "<html><body>GET: Rendezvous </body></html>"
+
+    def render_POST(self, request):
+        if self.name == 'gen_request':
+            content = request.content.read()
+            print 'POST: %s submitted %s\n' % (self.name, content)
+            return Rendezvous.freedom_request 
+        elif self.name == 'image':
+            imagefile = open("onion.jpg", "wb")
+            imagefile.write(request.content.read())
+            imagefile.close()
+            print 'POST: %s submitted %s\n' % (self.name, request.getHeader('Content-Length'))
+            return ''
+
