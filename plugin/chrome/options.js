9e62031e3516c965249168c1087f5c5303f8bab0
Ian A. Mason <Ian.Mason@SRI.com>
2013-09-18 15:37:41 -0700

    Options elaborated.

--- plugin/chrome/options.js
+++ plugin/chrome/options.js
@@ -9,66 +9,88 @@ Options = {
     options: { 
         
         server_name: {
-            getter: function () {       
-                return document.getElementById('server_name').value;
-            },
-            setter: function () { 
-                var value = localStorage['server_name'];
-                if(typeof value === 'string'){
-                    document.getElementById('server_name').value = value;
-                }
-            },
+            getter: function () { return Options.text_getter('server_name'); },
+            setter: function () { return Options.text_setter('server_name'); }
         },
         
         jumpbox_port: {
-            getter: function () {   
-                return document.getElementById('jumpbox_port').value;
-            },
-            
-            setter: function () {       
-                var value = localStorage['jumpbox_port'];
-                if(typeof value === 'string'){
-                    document.getElementById('jumpbox_port').value = value;
-                }
-            },
+            getter: function () { return Options.text_getter('jumpbox_port'); },
+            setter: function () { return Options.text_setter('jumpbox_port'); }
         },
 
         debug_mode: {
-            getter: function () {      
-                return document.getElementById('debug_mode').checked;
-            },
-            
-            setter: function () {       
-                var value = localStorage['debug_mode'];
-                if(typeof value === 'string'){
-                    document.getElementById('debug_mode').checked = (value === 'true');
-                }
-            }, 
-            
+            getter: function () { return Options.checkbox_getter('debug_mode'); },
+            setter: function () { return Options.checkbox_setter('debug_mode'); }
         },
+
         plugin_circuit_count: {
-            getter: function ( ) {  
-                var select = document.getElementById('plugin_circuit_count');
-                return select.children[select.selectedIndex].value;
-            },
-            
-            setter:  function ( ) {  
-                var value, select, index, child;
-                value = localStorage['plugin_circuit_count'];
-                if(typeof value === 'string'){
-                    select = document.getElementById('plugin_circuit_count');
-                    for (index = 0; index < select.children.length; index += 1) {
-                        child = select.children[index];
-                        if (child.value === value) {
-                            child.selected = 'true';
-                            return; 
-                        }
-                    }
+            getter: function () { return Options.select_getter('plugin_circuit_count'); },
+            setter: function () { return Options.select_setter('plugin_circuit_count'); }
+        },
+
+        stegotorus_log_level: {
+            getter: function () { return Options.select_getter('stegotorus_log_level'); },
+            setter: function () { return Options.select_setter('stegotorus_log_level'); }
+        },
+
+        stegotorus_circuit_count: {
+            getter: function () { return Options.select_getter('stegotorus_circuit_count'); },
+            setter: function () { return Options.select_setter('stegotorus_circuit_count'); }
+        },
+
+        stegotorus_steg_module: {
+            getter: function () { return Options.select_getter('stegotorus_steg_module'); },
+            setter: function () { return Options.select_setter('stegotorus_steg_module'); }
+        }
+
+    }, 
+    
+    text_getter: function(name){
+        return document.getElementById(name).value;
+    },
+
+    text_setter: function(name){
+        var value = localStorage[name];
+        if(typeof value === 'string'){
+            document.getElementById(name).value = value;
+        }
+    },
+
+    checkbox_getter: function(name){
+        return document.getElementById(name).checked;
+    },
+
+    checkbox_setter: function(name){
+        var value = localStorage[name];
+        if(typeof value === 'string'){
+            document.getElementById(name).checked = (value === 'true');
+        }    
+    },
+
+
+    select_getter: function(name){
+        var select = document.getElementById(name);
+        var value = select.children[select.selectedIndex].value;
+        console.log('select_getter: ' + name + ' = ' +  value);
+        return value;
+    },
+
+    select_setter: function(name){
+        var value, select, index, child;
+        value = localStorage[name];
+        console.log('select_setter: ' + name + ' = ' + value);
+        if(typeof value === 'string'){
+            select = document.getElementById(name);
+            for (index = 0; index < select.children.length; index += 1) {
+                child = select.children[index];
+                if (child.value === value) {
+                    child.selected = 'true';
+                    return; 
                 }
             }
         }
-    }, 
-    
+    },
+
     init: function () {
         Options.bkg = chrome.extension.getBackgroundPage();
         Options.restore_options();
